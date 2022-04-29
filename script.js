21c64ef739bd2b0dbd857562268a1668658e4247
Imane El Yakine <64931446+ImaneElYakine@users.noreply.github.com>
2022-04-29 14:57:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Evenement sur le scroll pour la rotation des cercles
 window.onscroll = function () {
     scrollRotate();
@@ -10,22 +12,5 @@ function scrollRotate() {
     document.getElementById("cercle2").style.transform = "rotate(-" + window.pageYOffset/4 + "deg)";
     document.getElementById("cercle4").style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
 }
-
-// Initialisation du Swiper des vidéos
-const galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
-
-// Initialisation du Swiper Qui sommes-nous pour le mobile
-const swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-    },
-});
-
 
 
